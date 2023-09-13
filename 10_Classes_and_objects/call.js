fe351d214151055a025753906e27b387908947d9
Siddharth Gupta <97101731+thesiddharth24@users.noreply.github.com>
2023-09-13 14:08:03 +0000

    call

---
+++ 10_Classes_and_objects/call.js
@@ -0,0 +1,16 @@
+function setUsername(username){
+    //complex DB calls 
+    this.username=username;
+}
+
+
+function createUser(username,email,password){
+    //setUsername(username);//ye abhi call nahi hua hai 
+    setUsername.call(username);//.call se refrence hold kark rakega 
+    this.email = email;
+    this.password = password;
+}
+
+const chai = new createUser("chai","chai@gmail.com");
+
+console.log(chai);
